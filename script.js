24cc996d6afdfee59a86e55bcd8d54c928e1ac91
yyynm22 <yminagawa@dl-techx-xedge.net>
2024-08-16 17:09:16 +0900

    資源のデプロイ

--- script.js
+++ script.js
@@ -3,47 +3,61 @@ const app = new Vue({
   vuetify: new Vuetify(),
   data: {
     // Vue内部で使いたい変数は全てこの中に定義する
-    temperature: '', //パラメーター「ID」格納変数
-    dress: '', //パラメーター「Name」格納変数
+    temperature: '', // パラメーター「temperature」格納変数
+    season: '', // パラメータ「season」格納変数
+    dress: '', // パラメーター「dress」格納変数
+    dressimg: '', // パラメータ「dressimg」格納変数
+    mark: '',//パラメータ「mark」格納変数
+    
     dataList: [], // データ表示用配列
   },
   methods: {
     // DBにデータを追加する関数
     addData: async function() {
-
-      //IDの入力チェック（空白か数字以外なら終了）
+      // IDの入力チェック（空白か数字以外なら終了）
       if(!this.temperature || isNaN(this.temperature)){
         console.log("temperatureに数値が入力されていません");
         return;
       }
       
-      //POSTメソッドで送るパラメーターを作成
+      // POSTメソッドで送るパラメーターを作成
       const param = {
         temperature : this.temperature,
-        dress : this.dress
+        season: this.season,
+        dress : this.dress,
+        dressimg: this.dressimg,
+        mark:this.mark,
       };
       
-      //INSERT用のAPIを呼び出し
-      const response = await axios.post('https://m3h-yuunaminagawa.azurewebsites.net/api/INSERT',param);
+      // INSERT用のAPIを呼び出し
+      const response = await axios.post('https://m3h-yuunaminagawa.azurewebsites.net/api/INSERT', param);
       
-      //結果をコンソールに出力
+      // 結果をコンソールに出力
       console.log(response.data);
-
+      
+      // 保存が完了したらフィールドをクリア
+      this.temperature = '';
+      this.season = '';
+      this.dress = '';
+      this.dressimg = '';
+      this.mark = '';
     },
+    
     // データベースからデータを取得する関数
     readData: async function() {
-      //SELECT用のAPIを呼び出し      
+      // SELECT用のAPIを呼び出し      
       const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT');
       
-      //結果をコンソールに出力
+      // 結果をコンソールに出力
       console.log(response.data);
       
-      //結果リストを表示用配列に代入
+      // 結果リストを表示用配列に代入
       this.dataList = response.data.List;
     },
   },
 });
-document.addEventListener('DOMContentLoaded', function(){
+
+document.addEventListener('DOMContentLoaded', function() {
   // タブに対してクリックイベントを適用
   const tabs = document.getElementsByClassName('tab');
   for(let i = 0; i < tabs.length; i++) {
@@ -51,10 +65,11 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   // タブをクリックすると実行する関数
-  function tabSwitch(){
+  function tabSwitch() {
     // タブのclassの値を変更
     document.getElementsByClassName('is-active')[0].classList.remove('is-active');
     this.classList.add('is-active');
+    
     // コンテンツのclassの値を変更
     document.getElementsByClassName('is-show')[0].classList.remove('is-show');
     const arrayTabs = Array.prototype.slice.call(tabs);
