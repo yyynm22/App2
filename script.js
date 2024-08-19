6f4daec55ef209e93dbd50f89dee9ce27ce998d5
yyynm22 <yminagawa@dl-techx-xedge.net>
2024-08-19 16:30:56 +0900

    Add2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const app = new Vue({
     temperature: '', // パラメーター「temperature」格納変数
     season: '', // パラメータ「season」格納変数
     dress: '', // パラメーター「dress」格納変数
-    dressimg: '', // パラメータ「dressimg」格納変数
+    dressURL: '', // パラメータ「dressimg」格納変数
     mark: '',//パラメータ「mark」格納変数
     
     dataList: [], // データ表示用配列
@@ -14,7 +14,7 @@ const app = new Vue({
   methods: {
     // DBにデータを追加する関数
     addData: async function() {
-      // temperatureの入力チェック（空白か数字以外なら終了）
+      // IDの入力チェック（空白か数字以外なら終了）
       if(!this.temperature || isNaN(this.temperature)){
         console.log("temperatureに数値が入力されていません");
         return;
@@ -22,11 +22,11 @@ const app = new Vue({
       
       // POSTメソッドで送るパラメーターを作成
       const param = {
-        temperature: this.temperature,
+        temperature : this.temperature,
         season: this.season,
-        dress: this.dress,
-        dressimg: this.dressimg,
-        mark: this.mark,
+        dress : this.dress,
+        dressURL: this.dressURL,
+        mark:this.mark,
       };
       
       // INSERT用のAPIを呼び出し
@@ -39,29 +39,22 @@ const app = new Vue({
       this.temperature = '';
       this.season = '';
       this.dress = '';
-      this.dressimg = '';
+      this.dressURL = '';
       this.mark = '';
     },
     
     // データベースからデータを取得する関数
-    readData: async function() {
-      // SELECT用のAPIを呼び出し      
-      const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT');
-      
-      // 結果をコンソールに出力
-      console.log(response.data);
-      
-      // 結果リストを表示用配列に代入
-      this.dataList = response.data.List;
-    },
+      readData: async function () {
+          // SELECT用のAPIを呼び出し      
+          const response = await axios.get('https://m3h-yuunaminagawa.azurewebsites.net/api/SELECT');
 
-    // データを削除する関数（deleteメソッド）
-    deleteData: function(index) {
-      if (this.dataList[index].isDone) {
-        this.doneTodos.push(this.dataList[index]);
-      }
-      this.dataList.splice(index, 1); // リストから項目を削除
-    }
+          // 結果をコンソールに出力
+          console.log(response.data);
+
+          // 結果リストを表示用配列に代入
+          this.dataList = response.data.List.sort((a, b) => a.temperature - b.temperature);
+      },
+
   },
 });
 
